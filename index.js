2bf026a3fe93c4953de5ac0ba80be9e40a1ce2ad
donnywarren <51206190+donnywarren@users.noreply.github.com>
2020-02-26 12:52:54 -0500

    first done

--- index.js
+++ index.js
@@ -12,11 +12,11 @@
 
 
 
-const givenVariable = 20;
-const doubledVariable = givenVariable * 2;
-const tripledVariable = givenVariable * 3;
-console.log(doubledVariable);
-console.log('EXAMPLE', 'tripledVariable', tripledVariable);
+// const givenVariable = 20;
+// const doubledVariable = givenVariable * 2;
+// const tripledVariable = givenVariable * 3;
+// console.log(doubledVariable);
+// console.log('EXAMPLE', 'tripledVariable', tripledVariable);
 // After running this and feeling comfortable, feel free to comment the above block of code out (either give each line a // or highlight the block of code and press COMMAND - / )
 
 // DATATYPES
@@ -36,6 +36,11 @@ console.log('EXAMPLE', 'tripledVariable', tripledVariable);
 
 // Set a constant named bestClassLowercase that converts bestClass to an all lowercase string.
 // Log this value to the console.
+const bestClass = "Supernovas";
+console.log(bestClass);
+let bestClassLowercase = bestClass.toLowerCase();
+console.log(bestClassLowercase);
+
 
 
 
