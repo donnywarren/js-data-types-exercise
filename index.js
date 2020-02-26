21c18f81f8ffbad2cce01b7cd2822c7ef23e0273
donnywarren <51206190+donnywarren@users.noreply.github.com>
2020-02-26 13:04:49 -0500

    mario

--- index.js
+++ index.js
@@ -131,6 +131,8 @@ console.log(grandHotelTremezzoGuests);
 // He is wearing a disguise...but orange crocs.
 // Log grandHotelTremezzoGuests to the console.
 
+grandHotelTremezzoGuests.push('Mario Batali');
+console.log(grandHotelTremezzoGuests);
 
 
 
